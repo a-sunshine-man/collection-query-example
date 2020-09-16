10e8c1c01544c87b0f1dde5af96118f9c0825202
qiany@isyscore.com <qiany@isyscore.com>
2020-09-16 13:38:47 +0800

    1.单个排序条件-非数字类型测试通过 2.单个排序条件-数字类型测试通过

--- src/test/java/com/yangyang/example/helper/CollectionCustomQueryHelperTest.java
+++ src/test/java/com/yangyang/example/helper/CollectionCustomQueryHelperTest.java
@@ -4,16 +4,24 @@ import com.yangyang.example.entity.Device;
 import com.yangyang.example.enums.WhereConnEnum;
 import com.yangyang.example.model.OrderWrapper;
 import com.yangyang.example.model.WhereWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+@Slf4j
 public class CollectionCustomQueryHelperTest {
     private CollectionCustomQueryHelper queryHelper;
     private static String DEV_NAME = "name-3";
@@ -87,17 +95,102 @@ public class CollectionCustomQueryHelperTest {
                 .forEach(e-> assertFalse(fieldVal == e.getActive() && DEV_NAME.equals(e.getDevName())));
     }
     /**
-     * 测试单个排序功能
+     * 测试单个排序功能-字符串
      */
     @Test
     public void queryAndSingletonSort(){
-        List<OrderWrapper> orders = Collections.singletonList(
-                OrderWrapper.build("secret", OrderWrapper.Condition.DESC)
-        );
+
+        /**String类型**/
+        OrderWrapper wrapper = OrderWrapper.build("devName", OrderWrapper.Condition.DESC);
+            //desc
+        List<OrderWrapper> orders = Collections.singletonList(wrapper);
         List<Device> list = queryHelper.query(null, orders, null);
         //返回的数据符合输入的条件
         assertNotNull(list);
         assertTrue(list.size() > 0);
+        //直接排序后对比
+        List<Device> listSource = new ArrayList<>(queryHelper.getDataList());
+        listSource.sort(Comparator.comparing(Device::getDevName).reversed());
+        for (int i = 0; i < listSource.size(); i++) {
+            Device devResult = list.get(i);
+            Device devSource = listSource.get(i);
+            assertEquals(devSource,devResult);
+        }
+            //asc
+        wrapper.setCondition(OrderWrapper.Condition.ASC);
+        list = queryHelper.query(null, orders, null);
+        //返回的数据符合输入的条件
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+        //直接排序后对比
+        listSource = new ArrayList<>(queryHelper.getDataList());
+        listSource.sort(Comparator.comparing(Device::getDevName));
+        for (int i = 0; i < listSource.size(); i++) {
+            Device devResult = list.get(i);
+            Device devSource = listSource.get(i);
+            assertEquals(devSource,devResult);
+        }
+
+        /**布尔类型**/
+        wrapper.setFieldName("active");
+            //asc
+        list = queryHelper.query(null, orders, null);
+        //返回的数据符合输入的条件
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+        //直接排序后对比
+        listSource = new ArrayList<>(queryHelper.getDataList());
+        listSource.sort(Comparator.comparing(Device::getActive));
+        for (int i = 0; i < listSource.size(); i++) {
+            Device devResult = list.get(i);
+            Device devSource = listSource.get(i);
+            assertEquals(devSource,devResult);
+        }
+            //desc
+        wrapper.setCondition(OrderWrapper.Condition.DESC);
+        list = queryHelper.query(null, orders, null);
+        //返回的数据符合输入的条件
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+        //直接排序后对比
+        listSource = new ArrayList<>(queryHelper.getDataList());
+        listSource.sort(Comparator.comparing(Device::getActive).reversed());
+        for (int i = 0; i < listSource.size(); i++) {
+            Device devResult = list.get(i);
+            Device devSource = listSource.get(i);
+            assertEquals(devSource,devResult);
+        }
+
+        /**数字类型**/
+        wrapper.setFieldName("devId");
+            //desc
+        list = queryHelper.query(null, orders, null);
+        //返回的数据符合输入的条件
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+        //直接排序后对比
+        listSource = new ArrayList<>(queryHelper.getDataList());
+        listSource.sort(Comparator.comparing(Device::getSecret).reversed());
+        for (int i = 0; i < listSource.size(); i++) {
+            Device devResult = list.get(i);
+            Device devSource = listSource.get(i);
+            assertEquals(devSource,devResult);
+        }
+            //asc
+        wrapper.setCondition(OrderWrapper.Condition.ASC);
+        list = queryHelper.query(null, orders, null);
+        //返回的数据符合输入的条件
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+        //直接排序后对比
+        listSource = new ArrayList<>(queryHelper.getDataList());
+        listSource.sort(Comparator.comparing(Device::getSecret));
+        for (int i = 0; i < listSource.size(); i++) {
+            Device devResult = list.get(i);
+            Device devSource = listSource.get(i);
+            assertEquals(devSource,devResult);
+        }
+
     }
 
 }
